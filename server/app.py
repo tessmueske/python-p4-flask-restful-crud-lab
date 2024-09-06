bcd0a2cdc4f667ebecfc2e9906e4b45848223bbc
Tess <tess.mueske@gmail.com>
2024-09-06 12:48:07 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -47,6 +47,29 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+        for attr in request.json:
+            setattr(plant, attr, request.json[attr])
+
+        db.session.commit()
+
+        response_dict = plant.to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            200
+        )
+
+        return response
+
+    def delete(self,id):
+        plant = Plant.query.filter(Plant.id == id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return '', 204
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
